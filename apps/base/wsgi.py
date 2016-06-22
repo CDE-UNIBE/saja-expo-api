dae0b399fd4a6af46f351c482fd9de9f0f0ca6b6
Sebastian Manger <s.manger@gmx.ch>
2016-06-22 12:52:36 +0200

    Example configurations for uwsgi and nginx

--- apps/base/wsgi.py
+++ apps/base/wsgi.py
@@ -6,11 +6,17 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
-from os.path import join, dirname, abspath
+import sys
 import envdir
+from os.path import join, dirname, abspath
 
-envdir.read(join(dirname(dirname(dirname(abspath(__file__)))), 'envs'))
+# Root directory for this project
+PROJECT_DIR = dirname(dirname(dirname(abspath(__file__))))
 
+# Add the apps folder to the path and read the env-vars.
+sys.path.append(join(PROJECT_DIR, 'apps'))
+envdir.read(join(PROJECT_DIR, 'envs'))
+
+# Load the wsgi application with django-configurations.
 from configurations.wsgi import get_wsgi_application
-
 application = get_wsgi_application()
