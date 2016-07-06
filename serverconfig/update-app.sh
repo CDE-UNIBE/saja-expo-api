6c655fb04f3a9e5e785e095160d394d99cc61a9e
Kurt Gerber <kurt.gerber@cde.unibe.ch>
2016-07-06 15:18:25 +0200

    update von update-app.sh

--- serverconfig/update-app.sh
+++ serverconfig/update-app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # activate virtualenv, run migrations and touch uwsgi to reload the application server.
-echo `date "+%F %T"` "Starting App update with git.."
+# echo `date "+%F %T"` "Starting App update with git.."
 APP_BASE=/srv/wnf-proxy-app
 APP_HOME=${APP_BASE}/app
 echo "updating proxy app"
@@ -9,4 +9,4 @@ git pull origin master
 source /srv/wnf-proxy-app/env/bin/activate  && python /srv/wnf-proxy-app/app/manage.py migrate
 sudo service uwsgi restart  
 sudo service nginx restart
-echo `date "+%F %T"` "App update done"
+# echo `date "+%F %T"` "App update done"
